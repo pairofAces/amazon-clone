aaa4a56284d9ededdb9a37975e3351a4f0940deb
pairofAces <karanchauhan16@gmail.com>
2020-11-24 22:47:29 -0500

    initialized Firebase connection

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDeU8esaGbb2-COgXlxK2WrseBEANnNAdA",
+  authDomain: "clone-d0660.firebaseapp.com",
+  databaseURL: "https://clone-d0660.firebaseio.com",
+  projectId: "clone-d0660",
+  storageBucket: "clone-d0660.appspot.com",
+  messagingSenderId: "202686432822",
+  appId: "1:202686432822:web:6988786a996d9eda05ba97",
+  measurementId: "G-FPJZG02FCX"
+};
