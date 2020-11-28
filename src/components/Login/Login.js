046e6e5e858b85fe258b2459bf7646a036550a47
pairofAces <karanchauhan16@gmail.com>
2020-11-28 15:02:30 -0500

    added white bg logo to login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Login.css';
+import logo from './Emporium-white.png';
 
 function Login() {
     return (
         <div className="login">
-            
+            <img src={logo} alt="Amazon Clone Logo"/>
         </div>
     )
 }
