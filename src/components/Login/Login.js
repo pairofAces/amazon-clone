47314b53025e81abf5c771f336a1d03a7f5bd65c
pairofAces <karanchauhan16@gmail.com>
2020-11-28 17:51:18 -0500

    logo goes to home

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Login.css';
 import logo from './Emporium-white.png';
+import { Link, useHistory } from 'react-router-dom';
 
 function Login() {
     return (
         <div className="login">
-            <img src={logo} alt="Amazon Clone Logo"/>
+            <Link to="/">
+            
+                <img 
+                    className="login_logo" 
+                    src={logo} 
+                    alt="Amazon Clone Logo"
+                />
+            </Link>
         </div>
     )
 }
