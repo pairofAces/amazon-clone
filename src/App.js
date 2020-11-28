4ace3dc01417bd1e90850ac130edffdccfe9a614
pairofAces <karanchauhan16@gmail.com>
2020-11-28 14:53:51 -0500

    fixed app.js navbar locations and working on login compnent

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/Header/Navbar';
 import Home from './components/Home/Home';
 import Checkout from './components/Checkout/Checkout';
+import Login from './components/Login/Login';
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <NavBar />
+        {/* <NavBar /> */}
         <Switch>
 
           <Route path="/login">
-            <h1>Login Page</h1>
+            <Login/>
+            {/* <h1>Login Page</h1> */}
           </Route>
 
           <Route path='/checkout'>
+            <NavBar />
             <Checkout />
           </Route>
 
           <Route path='/'>
+            <NavBar />
             <Home />
           </Route>
 
