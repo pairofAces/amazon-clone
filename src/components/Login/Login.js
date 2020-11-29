b891bf425fd575ece944ac3041e0b5143bc6042e
pairofAces <karanchauhan16@gmail.com>
2020-11-28 19:02:33 -0500

    created the sign-in box with inputs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,6 +14,27 @@ function Login() {
                     alt="Amazon Clone Logo"
                 />
             </Link>
+
+            <div className="login_container" >
+                <h1>Sign-In</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text"/>
+
+                    <h5>Password</h5>
+                    <input type="password"/>
+
+                    <button className="login_signInButton">Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to Emporium's Conditions of 
+                    Use & Sale. Please see our Privacy Notice, our Cookies
+                    Notice and our Interest-Based Ads.
+                </p>
+
+                <button className="login_registerButton">Create your Emporium Account</button>
+            </div>
         </div>
     )
 }
