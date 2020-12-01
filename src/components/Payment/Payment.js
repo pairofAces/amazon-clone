e9b9706dba1a9860c03037a55f124ac6133a1548
pairofAces <karanchauhan16@gmail.com>
2020-11-30 22:56:38 -0500

    added payment sections

---
+++ src/components/Payment/Payment.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Payment.css';
+
+function Payment() {
+    return (
+        <div className='payment'>
+            <div className="payment_container">
+                {/* Delivery address container */}
+                <div className="payment_section">
+
+
+                </div>
+                {/* Review Items container */}
+                <div className="payment_section">
+
+                    
+                </div>
+                {/* Payment Method container */}
+                <div className="payment_section">
+
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Payment
