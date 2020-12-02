03e70dd733df3ff336d10020b3b65ae0c0fe6fbc
pairofAces <karanchauhan16@gmail.com>
2020-12-02 01:19:26 -0500

    deployed

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     //the cloud function url (api) will be here
-    baseURL: 'http://localhost:5001/clone-d0660/us-central1/api'
+    baseURL: 'https://us-central1-clone-d0660.cloudfunctions.net/api'
+    // 'http://localhost:5001/clone-d0660/us-central1/api'
 });
 
 export default instance;
