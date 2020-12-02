f66a34b244d957ed714b7ed64e78cebb97811aba
pairofAces <karanchauhan16@gmail.com>
2020-12-02 00:15:40 -0500

    added useEffect

--- src/components/Order/Orders.js
+++ src/components/Order/Orders.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../Firebase/firebase';
 import './Orders.css';
+import { useStateValue } from '../Util/StateProvider';
 
 function Orders() {
+    const [{ basket, user }, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        if(user) {
+
+            db
+              .collection('users')
+              .doc(user?.uid)
+              .collection('orders')
+              .orderBy('created', 'desc')
+              .onSnapshot(snapshot => (
+                  setOrders(snapshot.docs.map(doc => ({
+                      id: doc.id,
+                      data: doc.data()
+                  })))
+              ))
+        } else {
+            setOrders([])
+        }
+
+    }, [user])
+
     return (
         <div className="orders">
             <h1>Your Orders</h1>
